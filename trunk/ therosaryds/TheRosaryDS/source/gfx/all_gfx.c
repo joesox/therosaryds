38c331c83c310f704750a6cad1b35f1457e326ca
joesox <joesox@2ecae7c1-3555-0410-9079-2b1433ebd601>
2008-09-20 14:40:10 +0000

    v1.3.1.0

---
+++ trunk/ therosaryds/TheRosaryDS/source/gfx/all_gfx.c
@@ -0,0 +1,81 @@
+//Gfx converted using Mollusk's PAGfx Converter
+
+//This file contains all the .c, for easier inclusion in a project
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "all_gfx.h"
+
+
+// Sprite files : 
+#include "resume_btn.c"
+#include "extras_btn.c"
+#include "next.c"
+#include "praywithme.c"
+#include "start_disabled.c"
+#include "whatistherosary_btn.c"
+#include "whatise.c"
+#include "creditsbutton.c"
+#include "creditsbuttone.c"
+#include "extrasbutton.c"
+#include "extrasbuttone.c"
+#include "startbutton.c"
+#include "startbuttone.c"
+#include "whatis.c"
+
+// Background files : 
+#include "WordScreen.c"
+#include "JSDSCopyright.c"
+#include "JSDSSplash.c"
+#include "newfont.c"
+#include "RosaryBottom.c"
+#include "RosaryBottomMix.c"
+#include "RosaryDSMenuBkg.c"
+#include "RosaryDSTitle.c"
+#include "RosaryTop.c"
+#include "RosaryTopMix.c"
+
+// Palette files : 
+#include "resume_btn.pal.c"
+#include "extras_btn.pal.c"
+#include "next.pal.c"
+#include "praywithme.pal.c"
+#include "start_disabled.pal.c"
+#include "WordScreen.pal.c"
+#include "JSDSCopyright.pal.c"
+#include "JSDSSplash.pal.c"
+#include "newfont.pal.c"
+#include "RosaryBottom.pal.c"
+#include "RosaryBottomMix.pal.c"
+#include "RosaryDSMenuBkg.pal.c"
+#include "RosaryDSTitle.pal.c"
+#include "RosaryTop.pal.c"
+#include "RosaryTopMix.pal.c"
+#include "whatistherosary_btn.pal.c"
+#include "whatise.pal.c"
+#include "creditsbutton.pal.c"
+#include "creditsbuttone.pal.c"
+#include "extrasbutton.pal.c"
+#include "extrasbuttone.pal.c"
+#include "startbutton.pal.c"
+#include "startbuttone.pal.c"
+#include "whatis.pal.c"
+
+// Background Pointers :
+PAGfx_struct WordScreen = {(void*)WordScreen_Map, 768, (void*)WordScreen_Tiles, 16384, (void*)WordScreen_Pal, (int*)WordScreen_Info };
+PAGfx_struct JSDSCopyright = {(void*)JSDSCopyright_Map, 768, (void*)JSDSCopyright_Tiles, 5184, (void*)JSDSCopyright_Pal, (int*)JSDSCopyright_Info };
+PAGfx_struct JSDSSplash = {(void*)JSDSSplash_Map, 768, (void*)JSDSSplash_Tiles, 45376, (void*)JSDSSplash_Pal, (int*)JSDSSplash_Info };
+PAGfx_struct RosaryBottom = {(void*)RosaryBottom_Map, 768, (void*)RosaryBottom_Tiles, 35584, (void*)RosaryBottom_Pal, (int*)RosaryBottom_Info };
+PAGfx_struct RosaryBottomMix = {(void*)RosaryBottomMix_Map, 768, (void*)RosaryBottomMix_Tiles, 36352, (void*)RosaryBottomMix_Pal, (int*)RosaryBottomMix_Info };
+PAGfx_struct RosaryDSMenuBkg = {(void*)RosaryDSMenuBkg_Map, 768, (void*)RosaryDSMenuBkg_Tiles, 49152, (void*)RosaryDSMenuBkg_Pal, (int*)RosaryDSMenuBkg_Info };
+PAGfx_struct RosaryDSTitle = {(void*)RosaryDSTitle_Map, 768, (void*)RosaryDSTitle_Tiles, 46912, (void*)RosaryDSTitle_Pal, (int*)RosaryDSTitle_Info };
+PAGfx_struct RosaryTop = {(void*)RosaryTop_Map, 768, (void*)RosaryTop_Tiles, 49152, (void*)RosaryTop_Pal, (int*)RosaryTop_Info };
+PAGfx_struct RosaryTopMix = {(void*)RosaryTopMix_Map, 768, (void*)RosaryTopMix_Tiles, 36992, (void*)RosaryTopMix_Pal, (int*)RosaryTopMix_Info };
+PAGfx_struct newfont = {(void*)newfont_Map, 768, (void*)newfont_Tiles, 4096, (void*)newfont_Pal, (int*)newfont_Info };
+
+#ifdef __cplusplus
+}
+#endif
+
